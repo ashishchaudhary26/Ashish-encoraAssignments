da9d576d63c8dba0552c74b3df19aaba30b7255f
Ashish.Chaudhary <Ashish.Chaudhary@ENCDAPPUNLT0719>
2025-10-07 15:42:16 +0530

    removal of extra import

--- Assignments/src/main/java/com/controller/UserController.java
+++ Assignments/src/main/java/com/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.service.annotation.GetExchange;
 
 import com.model.User;
 import com.repository.UserRepository;
